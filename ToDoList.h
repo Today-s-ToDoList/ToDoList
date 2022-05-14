5115e133563b0298dad3eaf15aa879f0beef2bf7
Dazunx <22100493.handong.ac.kr>
2022-05-14 17:53:03 +0900

    update ToDoList.h

--- ToDoList.h
+++ ToDoList.h
@@ -17,9 +17,9 @@ int readToDoList(ToDoList t); // 일정 목록 show (미완료 일정-중요도!
 int listToDoList(ToDoList *t[], int count); // 전체 등록된 제품 리스트 출력
 int updateToDoList(ToDoList *t); // 일정 수정
 int deleteToDoList(ToDoList *t[], int count); // 완료된 일정 체크(삭제)  : 완료된 일정의 중요도 0으로 설정
-void searchToDoList(ToDoList *t[], int count); // 제품이름 검색
-void saveData(ToDoList *t[],int count); // 날짜 파일 저장
-int loadData(ToDoList *t[]); // 날짜 파일을 읽어오기
+void searchToDoList(ToDoList *t[], int count); // 일정 검색
+void saveData(ToDoList *t[],int count); // 파일 저장
+int loadData(ToDoList *t[]); // 파일을 읽어오기
 int selectDataNo(ToDoList *t[], int count);
 int selectMenu(); // 메뉴 : 아래 함수들을 메뉴로 출력하는 함수
 struct tm currentTime(); // 현재 시간  : 현재 시간 불러오는 함수
